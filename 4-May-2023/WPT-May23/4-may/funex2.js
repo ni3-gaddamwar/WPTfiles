5f7f6db571a5b0af5c43eee86462ff0f6de23ea7
ni3-gaddamwar <131379838+ni3-gaddamwar@users.noreply.github.com>
2023-05-22 00:45:15 +0530

    Add files via upload

---
+++ 4-May-2023/WPT-May23/4-may/funex2.js
@@ -0,0 +1,8 @@
+function getAddition(num1,num2)
+{
+    let sum = num1 + num2
+    return sum
+}
+
+let v = getAddition(12,10)
+console.log("the sum is",v)
---
+++ 6-May-2023/6-may/lamdaex.js
@@ -0,0 +1,8 @@
+let s = (a,b)=> a+b   //lamda function is defined and assigned to variable s
+
+/* let v =s(12,10)
+console.log(v)
+ */
+
+console.log(s(12,10))   //function is called
+
